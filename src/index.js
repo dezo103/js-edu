10fd3228a0fb6ea394bb63e410102c2134aa2ffa
Aliaksandr Rudyk <dezo103@gmail.com>
2019-10-04 15:27:37 +0300

    add my solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-/**
+// /**
+//  * @param preferences - target student focus
+//  * @param knowsProgramming - if student can do programming and know basics
+//  * @param config - private student ability to perform for different focus modes
+//  * @returns number of weeks needed for finish education
+//  */
+// module.exports = function getTimeForEducation(
+//     focus = 'family', 
+//     knowsProgramming = true,
+//     config = {family: 4}
+//     ) {
+//       return 0;
+//   };
+  
+
+
+  /**
  * @param preferences - target student focus
  * @param knowsProgramming - if student can do programming and know basics
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = 'family', 
+  knowsProgramming = true,
+  config = {family: 4}
+  ) {
+
+    if (knowsProgramming == true){
+      return Math.ceil(800/config[focus]);
+      // it needs 800 hours
+    }
+      else{
+        //it needs 1300 hours
+        return Math.ceil(1300/config[focus]);
+      }
+
+};
